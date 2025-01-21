10534f87a04775509676c6a7c63b848e68d77464
mark0ht. <markus.teichert31@gmail.com>
2025-01-21 09:04:51 +0100

    added search js script

---
+++ components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="bg-gray-800 text-white p-4 text-center">
+      <p class="text-sm">
+        © 2025 Frühlingswetter Shop - Alle Rechte vorbehalten.
+      </p>
+    </footer>
+  </template>
+  
