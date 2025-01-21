dee3a2a5dc6bd75f4780614c73978a7f91ba336a
mark0ht. <markus.teichert31@gmail.com>
2025-01-21 11:02:01 +0100

    added searchbar.vue to components

---
+++ components/SearchBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="relative">
+      <!-- Search Input -->
+      <input
+        v-model="searchQuery"
+        @input="onSearch"
+        type="text"
+        placeholder="Search products..."
+        class="w-64 px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+      />
+      <!-- Clears button for search option   -->
+      <button
+        @click.prevent="clearSearch"
+        class="absolute right-1 top-1 h-8 w-8 flex items-center justify-center text-gray-500 hover:text-gray-700"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"
+             viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"/>
+        </svg>
+      </button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import { useSearchStore } from '~/store/search';
+  
+  // aaccess the search store
+  const searchStore = useSearchStore();
+  
+  // Local ref that syncs with the store
+  const searchQuery = ref(searchStore.searchTerm);
+  
+  // Called every time the input changes
+  const onSearch = () => {
+    searchStore.setSearchTerm(searchQuery.value);
+  };
+  
+  // Clears the sead input and store
+  const clearSearch = () => {
+    searchQuery.value = '';
+    searchStore.clearSearchTerm();
+  };
+  </script>
+  
