053c077f598f84fdb35ed5e583f620c13b82d78c
mark0ht. <markus.teichert31@gmail.com>
2025-01-22 19:40:09 +0100

    obi product linked to index.vue file

--- pages/index.vue
+++ pages/index.vue
@@ -19,13 +19,26 @@
             class="w-full h-48 object-cover rounded mb-4"
           />
           <h3 class="text-lg font-bold">{{ product.name }}</h3>
-          <p class="text-gray-600 mb-4">{{ product.price }}€</p>
-          <button
-            @click="addToCart(product)"
-            class="bg-blue-500 hover:bg-green-600 text-white px-4 py-2 rounded"
-          >
-            Warenkorb hinzufügen
-          </button>
+          <p v-if="product.price" class="text-gray-600 mb-4">{{ product.price }}€</p>
+
+          <div class="mt-4">
+            <button
+              v-if="!product.externalLink"
+              @click="addToCart(product)"
+              class="bg-blue-500 hover:bg-green-600 text-white px-4 py-2 rounded"
+            >
+              Warenkorb hinzufügen
+            </button>
+
+            <a
+              v-else
+              :href="product.externalLink"
+              target="_blank"
+              class="bg-green-500 hover:bg-green-700 text-white px-4 py-2 rounded inline-block text-center"
+            >
+              Extern kaufen
+            </a>
+          </div>
         </div>
       </div>
     </main>
@@ -33,10 +46,11 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import { useCartStore } from '~/store/cart';
 import { useSearchStore } from '~/store/search';
 
-// Example of   product images
+// Import product images
 import product1Image from '~/assets/images/khaki-2723896_1280.jpg';
 import product2Image from '~/assets/images/man-6795191_1280.jpg';
 
@@ -46,14 +60,23 @@ const searchStore = useSearchStore();
 const products = [
   { id: 1, name: 'Übergangsjacke1', price: 89.99, image: product1Image },
   { id: 2, name: 'Übergangsjacke2', price: 54.99, image: product2Image },
-  // ... more products
+  {
+    id: 3,
+    name: 'Goldfruchtpalme',
+    price: 29.99,
+    image: 'https://cdn.obi.de/product/images/6684419.jpg',
+    description: 'Höhe: 110-120 cm, Topf: ca. 24 cm, Areca Dypsis Lutescens',
+    externalLink: 'https://www.obi.de/p/6684419/goldfruchtpalme-hoehe-110-120-cm-topf-ca-24-cm-areca-dypsis-lutescens'
+  }
 ];
 
 const addToCart = (product) => {
-  cart.addToCart(product);
+  if (!product.externalLink) {
+    cart.addToCart(product);
+  }
 };
 
-// property filter for productss
+// Product filtering based on search term
 const filteredProducts = computed(() => {
   if (!searchStore.searchTerm) {
     return products;
