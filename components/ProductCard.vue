1228c787155083ef8383f48789036fffa9c8f06b
mark0ht. <markus.teichert31@gmail.com>
2025-01-22 16:49:12 +0100

    added component to support int and exter products to the page

---
+++ components/ProductCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="border rounded-lg p-4 shadow-md">
+      <img :src="product.image" :alt="product.name" class="w-full h-48 object-cover rounded-md" />
+      <h3 class="text-lg font-semibold mt-2">{{ product.name }}</h3>
+      <p v-if="product.description" class="text-gray-600">{{ product.description }}</p>
+      <p class="text-xl font-bold mt-2">{{ product.price }} €</p>
+  
+      <div class="mt-4">
+        <NuxtLink v-if="!product.externalLink" :to="`/product/${product.id}`" class="bg-blue-500 text-white px-4 py-2 rounded">
+          Details ansehen
+        </NuxtLink>
+        <a v-else :href="product.externalLink" target="_blank" class="bg-green-500 text-white px-4 py-2 rounded">
+          Extern kaufen
+        </a>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    product: Object
+  });
+  </script>
+  
