60517d19ff35bac628cc1d4b598c625eec227b89
mark0ht. <markus.teichert31@gmail.com>
2025-02-07 15:11:22 +0000

    sidebar for dashboard

---
+++ components/Sidebar.vue
@@ -0,0 +1,11 @@
+<template>
+    <aside class="w-64 bg-gray-800 text-white h-full">
+      <div class="p-4 font-bold text-lg">Admin Dashboard</div>
+      <ul>
+        <li class="p-3 hover:bg-gray-700"><NuxtLink to="/dashboard">Home</NuxtLink></li>
+        <li class="p-3 hover:bg-gray-700"><NuxtLink to="/products">Products</NuxtLink></li>
+        <li class="p-3 hover:bg-gray-700"><NuxtLink to="/orders">Orders</NuxtLink></li>
+      </ul>
+    </aside>
+  </template>
+  
